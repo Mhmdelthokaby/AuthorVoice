e36b633219e0677939e922a23d457cdafe8688dd
Mhmdelthokaby <melthokaby22@gmail.com>
2025-05-11 20:57:14 +0300

    s2

--- src/app/url.ts
+++ src/app/url.ts
@@ -1,3 +1,3 @@
-export const API_URL = 'https://theauthors.runasp.net/';
-export const Url = 'https://theauthors.runasp.net/'
+export const API_URL = 'https://theauthors.runasp.net/api/Author';
+export const Url = 'https://theauthors.runasp.net'
 export const PublishUrl = 'https://theauthors.runasp.net/api/PublishingHouse'
